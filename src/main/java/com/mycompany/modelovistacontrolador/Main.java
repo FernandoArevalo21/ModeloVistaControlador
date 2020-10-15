7a1c5f65095eb4949bcfd5bc0996d95c0cc47b04
FernandoArevalo21 <fernandoarevalo2000@hotmail.com>
2020-10-15 17:27:27 -0500

    Se crea la interfaz grafica

--- src/main/java/com/mycompany/modelovistacontrolador/Main.java
+++ src/main/java/com/mycompany/modelovistacontrolador/Main.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * clase encargad de mostrar el formulario
  */
 package com.mycompany.modelovistacontrolador;
 
+
+import vista.Vista;
 /**
- *
- * @author usuario
+ * @author Fernando Arevalo
+ * @author Johan Sanchez
+ * @since 15/10/2020
+ * @version 1.0.0
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+      
+        Vista view = new Vista();
+        view.setVisible(true);
+    }    
 }
